737d51df4fef383f466f5270a4e15393ae1a382e
hramn1 <stmihailov@yandex.ru>
2018-11-09 19:26:48 +0300

    верстка

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 'use strict';
-
+var swiperTestemonial = new Swiper('.swiper-container--testemonial', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
